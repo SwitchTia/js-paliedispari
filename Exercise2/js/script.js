ce42f3bb0189f4bd242aea53a447e42a4a1f16ec
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-03 15:26:37 +0100

    Feat: wip

--- Exercise2/js/script.js
+++ Exercise2/js/script.js
@@ -4,3 +4,25 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+const userInput = prompt("Even or odd?");
+console.log(userInput);
+const userNumberStr = prompt("Insert a number from 1 to 5");
+const userNumber = parseInt(userNumberStr);
+console.log(userNumber);
+
+function generateRandomNumbers(x){
+    const randomNumber = Math.floor(Math.random() * x) + 1;
+    return randomNumber;
+}
+const pcNumber = parseInt(generateRandomNumbers(5));
+console.log(pcNumber);
+
+function sum(x, y){
+    const result = x + y;
+    return result;
+}
+const sumOfNumbers = sum(userNumber, pcNumber);
+console.log(sumOfNumbers);
+
+
